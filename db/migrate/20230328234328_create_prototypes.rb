75e288d4adcdd72eae579e513dfd8b18e3e76330
soooooooora <127271130+soooooooora@users.noreply.github.com>
2023-03-31 14:15:41 +0900

    修正

--- db/migrate/20230328234328_create_prototypes.rb
+++ db/migrate/20230328234328_create_prototypes.rb
@@ -6,7 +6,7 @@ class CreatePrototypes < ActiveRecord::Migration[6.0]
       t.string :title,              null: false
       t.text :catch_copy,           null: false
       t.text :concept,              null: false
-      t.references :user,           null: false, foreign: true
+      t.references :user,           null: false, foreign_key: true
     end
   end
 end
