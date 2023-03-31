6593bfcc37ef4f85faed2c0e3af7e842a60b003d
soooooooora <127271130+soooooooora@users.noreply.github.com>
2023-03-31 13:58:16 +0900

    修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, except: [:index,:new,:create]
-  before_action :authenticate_user!, only: [:new,:edit,:destroy]
+  before_action :authenticate_user!, except: [:index,:show,]
   before_action :move_to_index, only: [:edit,:update,:destroy]
 
   def index
